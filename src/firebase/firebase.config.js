ecb0feeeae57157a0b87231696242dbd88886642
mohammedtakbir <takbirhossain678@gmail.com>
2022-11-15 16:55:56 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBATrsxkL0IPvyQg0bfUgZwssuHo6q2mM",
+  authDomain: "aircnc-cd57b.firebaseapp.com",
+  projectId: "aircnc-cd57b",
+  storageBucket: "aircnc-cd57b.appspot.com",
+  messagingSenderId: "459687835680",
+  appId: "1:459687835680:web:05d7fa36632774fa10a445"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
